f1bc3c85c7331636f8e4ba7dc3af7cb23ed54a50
Hudson Holanda <hudsonholanda55@gmail.com>
2022-08-15 19:40:20 -0300

    feat: import and usage Router component, import and usage Browser Router context and remove ContainerApp component

--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -1,11 +1,14 @@
+import { BrowserRouter } from 'react-router-dom'
 import { ThemeProvider } from 'styled-components'
+import { Router } from '../../Router'
 import { GlobalStyle } from '../../styles/global'
 import { defaultTheme } from '../../styles/themes/default'
-import { ContainerApp } from './styles'
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
-      <ContainerApp />
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
       <GlobalStyle />
     </ThemeProvider>
   )
