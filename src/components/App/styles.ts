1a14a1cb9b5ec7437829d09f2a6affc855efd6ef
Hudson Holanda <hudsonholanda55@gmail.com>
2022-08-15 19:39:39 -0300

    feat(refact): remove unused styles file

--- src/components/App/styles.ts
+++
@@ -1,7 +0,0 @@
-import styled from 'styled-components'
-import Background from '../../assets/images/background-pick-money-2-via-de-boletos.png'
-export const ContainerApp = styled.div`
-  background: url('${Background}') no-repeat center/cover;
-  height: 100vh;
-  width: 100%;
-`
