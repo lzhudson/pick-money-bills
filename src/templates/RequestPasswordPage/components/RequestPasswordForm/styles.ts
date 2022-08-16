7c8f29c6423a8fbfb0fb648d39deb2ddab58470a
Hudson Holanda <hudsonholanda55@gmail.com>
2022-08-15 21:18:40 -0300

    feat: create RequestPasswordForm component

---
+++ src/templates/RequestPasswordPage/components/RequestPasswordForm/styles.ts
@@ -0,0 +1,12 @@
+import styled from 'styled-components'
+
+export const RequestPasswordContainer = styled.form`
+  margin-top: 2.5rem;
+  @media (max-width: 425px) {
+    margin-top: 4.05rem;
+  }
+  button[type='submit'] {
+    margin-top: 1.5rem;
+    width: 100%;
+  }
+`
