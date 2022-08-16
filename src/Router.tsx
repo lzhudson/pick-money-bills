59f724a91aa8dfb4b6501ae4771ed304e8ce8543
Hudson Holanda <hudsonholanda55@gmail.com>
2022-08-15 21:17:26 -0300

    feat: add new route request password

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,10 +1,12 @@
 import { Routes, Route } from 'react-router-dom'
 import { Login } from './pages/Login'
+import { RequestPassword } from './pages/RequestPassword'
 
 export function Router() {
   return (
     <Routes>
       <Route path="/" element={<Login />} />
+      <Route path="/solicitar-senha" element={<RequestPassword />} />
     </Routes>
   )
 }
