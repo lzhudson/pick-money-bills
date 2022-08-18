257bba1bc81d0bc9ca5faff873b0b77542026fd8
Hudson Holanda <hudsonholanda55@gmail.com>
2022-08-17 23:36:53 -0300

    feat: create Header component

---
+++ src/templates/BillsListPage/components/Header/styles.ts
@@ -0,0 +1,51 @@
+import styled from 'styled-components'
+
+export const HeaderContainer = styled.header`
+  background-color: #f9f9f9;
+  display: flex;
+  justify-content: space-between;
+  h1 {
+    color: ${(props) => props.theme['green-700']};
+    font-family: ${(props) => props.theme.caecillia};
+  }
+  p {
+    font-family: ${(props) => props.theme.openSans};
+  }
+`
+
+export const HeaderContentContainer = styled.div`
+  max-width: 1065px;
+  width: 100%;
+  margin: 0 auto;
+  display: flex;
+  justify-content: space-between;
+  padding: 22px 10px;
+  align-items: center;
+  @media (max-width: 425px) {
+    flex-direction: column;
+    align-items: flex-start;
+    padding: 30px 42px;
+  }
+`
+
+export const UserInfo = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 1.25rem;
+  p {
+    font-size: 20px;
+  }
+`
+
+export const LogoutContainer = styled.div`
+  @media (max-width: 425px) {
+    width: 100%;
+    margin-top: 36px;
+  }
+  button {
+    width: 230px;
+    @media (max-width: 425px) {
+      width: 100%;
+    }
+  }
+`
