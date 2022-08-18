bf1f676cfa32d2471ff29635c06413ab3456bb02
Hudson Holanda <hudsonholanda55@gmail.com>
2022-08-17 23:35:52 -0300

    feat: add route bills list

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,4 +1,5 @@
 import { Routes, Route } from 'react-router-dom'
+import { BillsList } from './pages/BillsList'
 import { Login } from './pages/Login'
 import { LostPassword } from './pages/LostPassword'
 import { PasswordSended } from './pages/PasswordSended'
@@ -11,6 +12,7 @@ export function Router() {
       <Route path="/solicitar-senha" element={<RequestPassword />} />
       <Route path="/esqueceu-sua-senha" element={<LostPassword />} />
       <Route path="/senha-enviada" element={<PasswordSended />} />
+      <Route path="/lista-de-boletos" element={<BillsList />} />
     </Routes>
   )
 }
