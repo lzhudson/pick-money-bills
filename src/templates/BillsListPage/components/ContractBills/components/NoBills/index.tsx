0e8de0fb0cea950e19b3e5119c45064b98a06af0
Hudson Holanda <hudsonholanda55@gmail.com>
2022-08-19 09:49:27 -0300

    feat: create NoBills component

---
+++ src/templates/BillsListPage/components/ContractBills/components/NoBills/index.tsx
@@ -0,0 +1,9 @@
+import { NoBillsContainer } from './styles'
+
+export function NoBills() {
+  return (
+    <NoBillsContainer>
+      <p>Este contrato não possui boletos em aberto.</p>
+    </NoBillsContainer>
+  )
+}
