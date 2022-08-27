542d5576cd1f002ac2f0d75aea39dba544eaffc1
Hudson Holanda <hudsonholanda55@gmail.com>
2022-08-27 00:04:49 -0300

    feat: usage AuthProvider

--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -1,15 +1,18 @@
 import { BrowserRouter } from 'react-router-dom'
 import { ThemeProvider } from 'styled-components'
+import { AuthProvider } from '../../hooks/auth'
 import { Router } from '../../Router'
 import { GlobalStyle } from '../../styles/global'
 import { defaultTheme } from '../../styles/themes/default'
 export function App() {
   return (
-    <ThemeProvider theme={defaultTheme}>
-      <BrowserRouter>
-        <Router />
-      </BrowserRouter>
-      <GlobalStyle />
-    </ThemeProvider>
+    <AuthProvider>
+      <ThemeProvider theme={defaultTheme}>
+        <BrowserRouter>
+          <Router />
+        </BrowserRouter>
+        <GlobalStyle />
+      </ThemeProvider>
+    </AuthProvider>
   )
 }
