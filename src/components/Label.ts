7ae35cc6da08c79fa873c7b3fd293ca05682130d
Hudson Holanda <hudsonholanda55@gmail.com>
2022-08-27 00:05:00 -0300

    feat: add error state in label

--- src/components/Label.ts
+++ src/components/Label.ts
@@ -1,9 +1,19 @@
-import styled from 'styled-components'
+import styled, { css } from 'styled-components'
 
-export const Label = styled.label`
+interface ILabelProps {
+  error: boolean
+}
+
+export const Label = styled.label<ILabelProps>`
   font-family: ${(props) => props.theme.caecillia};
   font-weight: 700;
   font-size: 14px;
   text-transform: uppercase;
   color: ${(props) => props.theme['green-700']};
+
+  ${(props) =>
+    props.error &&
+    css`
+      color: ${(props) => props.theme['red-500']};
+    `}
 `
