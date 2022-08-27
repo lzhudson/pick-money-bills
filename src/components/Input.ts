1842c3314e7edfcb846ea3a0b50f65bae40c5a51
Hudson Holanda <hudsonholanda55@gmail.com>
2022-08-27 00:05:06 -0300

    feat: add error state in input

--- src/components/Input.ts
+++ src/components/Input.ts
@@ -1,6 +1,16 @@
-import styled from 'styled-components'
+import styled, { css } from 'styled-components'
 
-export const Input = styled.input`
+interface IInputProps {
+  error: boolean
+}
+
+export const Input = styled.input<IInputProps>`
   border: 1px solid ${(props) => props.theme['gray-900']};
   padding: 0.95rem 1rem;
+
+  ${(props) =>
+    props.error &&
+    css`
+      border-color: ${(props) => props.theme['red-500']};
+    `}
 `
