5c5d8a554a57ba1f79945e1c0b836372bcbd7f13
Hudson Holanda <hudsonholanda55@gmail.com>
2022-09-23 10:15:15 -0300

    refact: remove mask

--- src/templates/LoginPage/components/LoginForm/index.tsx
+++ src/templates/LoginPage/components/LoginForm/index.tsx
@@ -1,31 +1,15 @@
-import { ChangeEvent, useState } from 'react'
-import { Controller } from 'react-hook-form'
 import { Link } from 'react-router-dom'
 import { ToastContainer } from 'react-toastify'
 import { AlertMessage } from '../../../../components/AlertMessage'
 import { Button } from '../../../../components/Button'
 import { Input } from '../../../../components/Input'
 import { InputGroup } from '../../../../components/InputGroup'
-import { InputMasked } from '../../../../components/InputMasked'
 import { Label } from '../../../../components/Label'
 import { useLoginForm } from './hooks'
 import { ButtonsContainer, LoginFormContainer } from './styles'
 
 export function LoginForm() {
-  const { register, onSubmit, errors, isLoading, control } = useLoginForm()
-  const [mask, setMask] = useState('')
-
-  function handleInputDocumentNumberChange(e: ChangeEvent<HTMLInputElement>) {
-    if (e.target.value || e.target.value.length === 0) {
-      setMask('999999999999999999')
-    }
-
-    if (e.target.value.replace(/\D/g, '').length < 12) {
-      setMask('999.999.999-999')
-    } else {
-      setMask('99.999.999/9999-99')
-    }
-  }
+  const { register, onSubmit, errors } = useLoginForm()
   return (
     <LoginFormContainer>
       <form onSubmit={onSubmit}>
@@ -33,31 +17,13 @@ export function LoginForm() {
           <Label error={!!errors.documentNumber} htmlFor="document-number">
             CPF/CNPJ
           </Label>
-          <Controller
-            control={control}
-            name="documentNumber"
-            render={({
-              field: { onChange, onBlur, value, name, ref },
-              fieldState: { invalid, isTouched, isDirty, error },
-              formState,
-            }) => (
-              <>
-                <InputMasked
-                  error={!!errors.documentNumber}
-                  mask={mask}
-                  id="document-number"
-                  placeholder="CPF/CNPJ"
-                  onBlur={onBlur}
-                  onChange={(e) => {
-                    handleInputDocumentNumberChange(e)
-                    onChange(e)
-                  }}
-                  value={value}
-                  inputRef={ref}
-                  name={name}
-                />
-              </>
-            )}
+          <Input
+            maxLength={18}
+            error={!!errors.documentNumber}
+            type="text"
+            id="document-number"
+            placeholder="CPF/CNPJ"
+            {...register('documentNumber')}
           />
         </InputGroup>
         <InputGroup>
