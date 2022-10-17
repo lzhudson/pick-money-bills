2067812fbd8d8f88a513f3e780b2a3217c1c5657
Hudson Holanda <hudsonholanda55@gmail.com>
2022-10-17 17:42:58 -0300

    feat: styles button

--- src/templates/RequestPasswordPage/styles.ts
+++ src/templates/RequestPasswordPage/styles.ts
@@ -7,6 +7,16 @@ export const RequestPasswordContainer = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
+
+  .link-back {
+    display: block;
+    width: 100%;
+    margin: 0 auto;
+    margin-top: 1rem;
+    button {
+      width: 100%;
+    }
+  }
 `
 
 export const RequestPasswordContentBox = styled.div`
